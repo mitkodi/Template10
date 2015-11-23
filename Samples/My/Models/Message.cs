837643a1e3c1dcf9c455482b9eb04a4a7d66171b
dimitar dimitrov <mitkodi@live.com>
2015-11-23 18:18:46 +0200

    Adding Message Service.

---
+++ Samples/My/Models/Message.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template10.Mvvm;
+
+namespace MyTest.Models {
+	public class Message : BindableBase {
+		private string _id = default(string);
+		public string Id { get { return _id; } set { Set(ref _id, value); } }
+
+		private string _from = default(string);
+		public string From { get { return _from; } set { Set(ref _from, value); } }
+
+		private string _to = default(string);
+		public string To { get { return _to; } set { Set(ref _to, value); } }
+
+		private string _subject = default(string);
+		public string Subject { get { return _subject; } set { Set(ref _subject, value); } }
+
+		private string _body = default(string);
+		public string Body { get { return _body; } set { Set(ref _body, value); } }
+
+		private DateTime _date = default(DateTime);
+		public DateTime Date { get { return _date; } set { Set(ref _date, value); } }
+
+		private Boolean _isRead = false;
+		public Boolean IsRead { get { return _isRead; } set { Set(ref _isRead, value); } }
+	}
+}
---
+++ Samples/My/Services/MessageServices/MessageService.cs
@@ -0,0 +1,34 @@
+﻿using MyTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template10.Utils;
+
+namespace MyTest.Services.MessageServices {
+	public partial class MessageService {
+		private ObservableCollection<Message> _messages;
+
+		public ObservableCollection<Message> GetMessages() {
+			if (_messages == null) {
+				_messages = new ObservableCollection<Message>(GetSampleData());
+			}
+
+			return _messages;
+		}
+
+		public ObservableCollection<Message> Search(string value) => GetMessages()
+			.Where(m => m.From.ToLower().Contains(value?.ToLower() ?? String.Empty))
+			.Where(m => m.Subject.ToLower().Contains(value?.ToLower() ?? String.Empty))
+			.Where(m => m.Body.ToLower().Contains(value?.ToLower() ?? String.Empty))
+			.ToObservableCollection();
+
+		public Message GetMessage(string id) => GetMessages().FirstOrDefault(m => m.Id.Equals(id));
+
+		public void DeleteMessage(Message message) {
+			GetMessages().Remove(message);
+		}
+	}
+}
