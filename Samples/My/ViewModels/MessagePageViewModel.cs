d1e1d769e36c0e47f80353fa78a96456d3f71c4c
dimitar dimitrov <mitkodi@live.com>
2015-11-25 01:42:31 +0200

    Adding Message(s) ViewModels and empty pages.

---
+++ Samples/My/ViewModels/MessagePageViewModel.cs
@@ -0,0 +1,97 @@
+﻿using MyTest.Models;
+using MyTest.Services.MessageServices;
+using MyTest.Views;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Template10.Mvvm;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Navigation;
+using System.Threading.Tasks;
+using Template10.Services.NavigationService;
+
+namespace MyTest.ViewModels {
+	public class MessagePageViewModel : Mvvm.ViewModelBase {
+		private MessageService _messageService;
+
+		private ObservableCollection<Message> _messages;
+		public ObservableCollection<Message> Messages { get { return _messages; } set { Set(ref _messages, value); } }
+
+		public MessageViewModel SelectedMessageViewModel { get; } = new MessageViewModel();
+
+		private DelegateCommand<string> _goToMessagePageCommand;
+		public ICommand GoToMessagePageCommand {
+			get {
+				return _goToMessagePageCommand ?? (_goToMessagePageCommand = new DelegateCommand<string>(
+					(id) => {
+						if (!String.IsNullOrEmpty(id)) {
+							NavigationService.Navigate(typeof(MessagePage), id);
+						}
+					},
+					(id) => !String.IsNullOrEmpty(id)
+				));
+			}
+		}
+
+		private string _searchValue;
+		public string SearchValue {
+			get { return _searchValue; }
+			set {
+				if (Set(ref _searchValue, value)) {
+					_searchCommand.RaiseCanExecuteChanged();
+				}
+
+			}
+		}
+
+		private DelegateCommand _searchCommand;
+		public ICommand SearchCommand {
+			get {
+				return _searchCommand ?? (_searchCommand = new DelegateCommand(
+					() => {
+						if (!String.IsNullOrEmpty(SearchValue)) {
+							Messages = _messageService.Search(SearchValue);
+							_clearSearchCommand.RaiseCanExecuteChanged();
+						}
+					},
+					() => !String.IsNullOrEmpty(SearchValue)
+				));
+			}
+		}
+
+		private DelegateCommand _clearSearchCommand;
+		public ICommand ClearSearchCommand {
+			get {
+				return _clearSearchCommand ?? (_clearSearchCommand = new DelegateCommand(
+					() => {
+						if (!String.IsNullOrEmpty(SearchValue)) {
+							Messages = _messageService.GetMessages();
+							SearchValue = null;
+							_searchCommand.RaiseCanExecuteChanged();
+						}
+					},
+					() => !String.IsNullOrEmpty(SearchValue)
+				));
+			}
+		}
+
+		public override void OnNavigatedTo(object parameter, NavigationMode mode, IDictionary<string, object> state) {
+			base.OnNavigatedTo(parameter, mode, state);
+		}
+
+		public override Task OnNavigatedFromAsync(IDictionary<string, object> state, bool suspending) {
+			if (SelectedMessageViewModel.Message != null) {
+				state["messageId"] = SelectedMessageViewModel.Message.Id;
+			}
+			if (!String.IsNullOrEmpty(SearchValue)) {
+				state["searchValue"] = SearchValue;
+			}
+
+			return Task.FromResult<object>(null);
+		}
+
+		public override void OnNavigatingFrom(NavigatingEventArgs args) {
+			args.Cancel = false;
+		}
+	}
+}
---
+++ Samples/My/ViewModels/MessageViewModel.cs
@@ -0,0 +1,75 @@
+﻿using MyTest.Models;
+using MyTest.Services.MessageServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Template10.Mvvm;
+using Windows.UI.Xaml.Navigation;
+using Template10.Services.NavigationService;
+
+namespace MyTest.ViewModels {
+	public class MessageViewModel : Mvvm.ViewModelBase {
+		private MessageService _messageService;
+		protected bool Standalone;
+
+		private Message _message;
+		public Message Message { get { return _message; } set { Set(ref _message, value); } }
+
+		private DelegateCommand _deleteCommand;
+		public ICommand DeleteCommand {
+			get {
+				return _deleteCommand ?? (_deleteCommand = new DelegateCommand(
+					() => {
+						if (Message != null) {
+							_messageService.DeleteMessage(Message);
+						}
+						Message = null;
+						if (Standalone) {
+							NavigationService.GoBack();
+						}
+						else {
+							_deleteCommand.RaiseCanExecuteChanged();
+						}
+					},
+					() => Message != null
+				));
+			}
+		}
+
+		public MessageViewModel() {
+			_messageService = new MessageService();
+		}
+
+		public override void OnNavigatedTo(object parameter, NavigationMode mode, IDictionary<string, object> state) {
+			string messageId = null;
+			if (parameter != null) {
+				messageId = parameter.ToString();
+			}
+			else {
+				if (state.ContainsKey("messageId")) {
+					messageId = state["messageId"].ToString();
+				}
+			}
+
+			if (messageId != null) {
+				Message = _messageService.GetMessage(messageId);
+			}
+		}
+
+		public override Task OnNavigatedFromAsync(IDictionary<string, object> state, bool suspending) {
+			if (suspending) {
+				if (Message != null) {
+					state["messageId"] = Message.Id;
+				}
+			}
+			return Task.FromResult<object>(null);
+		}
+
+		public override void OnNavigatingFrom(NavigatingEventArgs args) {
+			args.Cancel = false;
+		}
+	}
+}
